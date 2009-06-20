701dfe1dfb335650d36fa56e7d369ff61f293a34
matveev <matveev@theochem.tu-muenchen.de>
2009-06-20 14:24:54 +0000

    GXFILE: gxread/gxwrite return/accept gxfile columns and energy

--- ase/optimize/gxfile.py
+++ ase/optimize/gxfile.py
@@ -6,8 +6,9 @@ from sys import stdout
 from warnings import warn
 
 # to wrap the 3D vectors as V([x,y,z]):
-from vector import Vector as V
-# from numpy import array as V
+# from vector import Vector as V
+import numpy as np
+from numpy import array as V
 # from derivatives import DerivVector as V
 
 # Dummy atoms have to be distinguished from real  ones.
@@ -16,184 +17,20 @@ from vector import Vector as V
 # atoms to be identified by this:
 DUMMY = 0
 
-class GxAtom:
-    """The record type to hold information from the gxfile
-
-    Attributes:
-    charge :: Atomic number
-    pos    :: V([float,float,float]) -- postition of the atom
-    id     :: int -- zero-based id used to define connectivities
-
-    bond   :: ((int,int),int) -- ids of atoms defining the bond and bond id
-    angle  :: ((int,int,int),int) -- ids of atoms defining the angle and angle id
-    plane  :: ((int,int,int,int),int) -- ids of atoms defining the plane and plane id
-              The first int in the last three fields is the
-              id of the atom itself.
-
-              bond[1]  -- zero based index of variable bond
-              angle[1] -- zero based index of variable bond angle
-              plane[1] -- zero based index of variable dihedral angle
-
-              These fields are -1 for if the corresponding coordinate
-              is fixed, the indices for symmetry-equivalent variables
-              are identical
-
-    grad   :: V([float,float,float]) -- cartesian energy gradient wrt
-              atomic position. Note that grad == (-force) !
-
-    num    :: int -- zero-based index of real atoms excluding dummies
-              Dont use it for indexing into array of atoms, the array
-              does include dummies. Use |id| instead.
-    """
-    def __str__(self):
-        # dir(obj) returns a list of all object members,
-        # filter out those starting with '_':
-        records = filter( lambda x: x[0] != '_', dir(self) )
-        out = ""
-        for rec in records:
-            if out: out += ", " # not in the first iteration
-            out += rec + "=" + str(getattr(self,rec))
-        return out
-    #end def
-
-    def _tostr_geom(self):
-        """Return a line of gxfile in following format:
-        ^ 1.00        -5.184294677816         2.993153927796         0.427911418660   5  18     6   1   3     4   7   0
-        """
-
-        # extract the fields from records:
-
-        if is_dummy(self):
-            # gxfile convention for dummy atoms:
-            charge = 99.0
-        else:
-            charge = self.charge
-
-        # conversion to floats is for the case when vector components are DerivVars:
-        pos    = tuple( map(float, self.pos) )
-        unique = self.unique
-        id     = self.id
-
-        # connectivities (three fields):
-        conn   = self.plane[0][1:]
-
-        # variable indices (three fields):
-        vars   = (self.bond[1], self.angle[1], self.plane[1])
-
-        #
-        # WARNING: all the numerical ids are rebased to 1,
-        #          so that those (invalid) -1s become 0.
-        #
-
-        unique += 1
-        id     += 1
-        conn    = tuple([ i + 1 for i in conn])
-        vars    = tuple([ i + 1 for i in vars])
-
-        # concatenate tuples, the first one is a singleton:
-        fields = (charge,) + pos + (unique, id) + conn + vars
-
-        return ( "%5.2f %22.12f %22.12f %22.12f %3i %3i   %3i %3i %3i   %3i %3i %3i\n" \
-                     % fields )
-    #end def
-
-    def _tostr_grad(self):
-        """Return a line of gxfile containg cartesian gradients:
-        ^   20       -0.000000596917  -0.000000344630   0.000002806462
-        """
-        #
-        # WARNING: all the numerical ids are rebased to 1,
-        #          so that those (invalid) -1s become 0.
-        #
-
-        num  = self.num + 1
-
-        # conversion to floats is for the case when vector components are DerivVars:
-        grad = tuple( map(float, self.grad) )
-
-        # concatenate tuples, the first one is a singleton:
-        fields = (num,) + grad
-        return ( "%5i      %16.12f %16.12f %16.12f\n" % fields )
-    #end def
-
-    def _parse_geom(self,line):
-        """Parse a line of gxfile in following format:
-        ^ 1.00        -5.184294677816         2.993153927796         0.427911418660   5  18     6   1   3     4   7   0
-        """
-        fields = line.split()
-        # print(fields)
-
-        # nuclear charge, may even be fractional, for dummy atoms Z=99:
-        charge = int( float( fields[0] ) )
-
-        # gxfile convention for dummy atoms:
-        if charge == 99.0: charge = DUMMY
-
-        self.charge = charge
-
-        # exit on negative charge:
-        if charge < 0:
-            # this is the only usefull field in this line,
-            # but there is no use for it here ...
-            return
-
-        # 3D vector of atomic position:
-        self.pos    = [ float(i) for i in fields[1:4] ] # yes, three fields 1 <= xyz < 4
-
-        # Vrap 3D vectors in vector type, see "import ... as V" at the top:
-        self.pos = V(self.pos)
-
-        #
-        # WARNING: all the numerical ids are rebased to zero,
-        #          so that those (invalid) zeros become -1.
-        #          This is to use them for indexing into arrays/lists.
-        #
-
-        # uniqie atom id, indexing groups of symmetry equivalent atoms:
-        self.unique = int( fields[4] ) - 1
-
-        # numeric ID used to define connectiviites:
-        self.id     = int( fields[5] ) - 1
-
-        # connectivities:
-        conn        = [ int(i) - 1 for i in fields[6:9] ] # three fields
-
-        # integer lables for "variable" internal coordinates,
-        # zero for "constrained" internal coordinates:
-        vars        = [ int(i) - 1 for i in fields[9:12] ] # three fields
-
-        bond        = ( self.id, conn[0] )
-        angle       = ( self.id, conn[0], conn[1] )
-        plane       = ( self.id, conn[0], conn[1], conn[2] )
-
-        self.bond   = ( bond , vars[0] )
-        self.angle  = ( angle, vars[1] )
-        self.plane  = ( plane, vars[2] )
-    #enddef
-
-    def _parse_grad(self,line):
-        """Parse a line of gxfile containg cartesian gradients:
-        ^   20       -0.000000596917  -0.000000344630   0.000002806462
-        """
-        fields = line.split()
-        self.grad = map( float, fields[1:4] )
-
-        # Vrap 3D vectors in vector type, see "import ... as V" at the top:
-        self.grad = V(self.grad)
-    #enddef
-#end class
-
 # tiny helper function:
-def is_dummy(atom):
-    return atom.charge == DUMMY
+def is_dummy(atnum):
+    return atnum == DUMMY
 
 # for private use only:
 class EOF(Exception): pass
 
-def gxread(file):
-    """Read gxfile in following format,
+def gxread( file='gxfile' ):
+    """Read gxfile in following format, return a list of lists (columns)
+    that is a table in the row-major order.
+    So far only the geometry section is returned.
+
     (first and last lines are not parts of the file, rather for field width extimation):
-    #12345 1234567890123456789012 1234567890123456789012 1234567890123456789012 123 123   123 123 123   123 123 123
+    ^12345 1234567890123456789012 1234567890123456789012 1234567890123456789012 123 123   123 123 123   123 123 123
     ^92.00         0.000000000000         0.000000000000         0.000000000000   1   1     0   0   0     0   0   0
     ^99.00         1.000000000000         0.000000000000         0.000000000000   0   2     1   0   0     0   0   0
     ^ 8.00         0.000000000000         0.000000000000         3.354241540616   2   3     1   2   0     1   0   0
@@ -211,51 +48,44 @@ def gxread(file):
     ^   ...
     ^   20       -0.000000596917  -0.000000344630   0.000002806462
     ^   21        0.000000000000   0.000000689261   0.000002806462
-    #12345      1234567890123456 1234567890123456 1234567890123456
+    ^12345      1234567890123456 1234567890123456 1234567890123456
      """
-    atoms = []
 
-    # iterator over lines in a file:
+    # a generator function:
+    def parse(lines):
+	# iterator over lines in a file:
+	for line in lines:
+	    fields = _parse_geom(line)
+	    # stop iterations on negative atomic numbers:
+	    if fields[0] < 0:  return
+	    yield fields
+
+    # a generator for lines of the file:
     lines = open(file)
 
-    #
-    # First,  read in geometry:
-    #
-    numall = 0 # running atom counter
-    num = 0 # not counting dummies
-    for line in lines:
-        # new empty record:
-        atom = GxAtom()
+    # force generator to list:
+    rows = list( parse(lines) )
+    # print "rows=", rows
+    
+    # rearrange rows to columns:
+    cols = zip( *rows )
 
-        # parse a line, saving fields as records of GxAtom:
-        atom._parse_geom(line)
+    # unpack, we will change the type of "xyz":
+    atnums, xyz, isyms, inums, iconns, ivars = cols
 
-        # exit on negative charge:
-        if atom.charge < 0:
-            # this is the only usefull field in this line:
-            loop = - atom.charge
-            break
-        #end if
+    # helper funciton to convert a list of 3-vectors to Nx3 numpy array:
+    def nx3(xyz):
+	# FIXME: how to convert a list of numpy arrays to numpy array?:
+	n = len(xyz)
+	arr = np.zeros((n, 3))
+	for i in xrange(n):
+	    arr[i,:] = xyz[i]
+	return arr
+    #enddef
 
-        # otherwise this seems to be a valid atom:
-        numall += 1
-
-        # but some of them are dummy atoms:
-        if is_dummy(atom):
-            atom.num = -1
-        else:
-            num += 1
-            # sequence index, zero-based:
-            atom.num = num - 1
-        #end if
-
-        # not usual, therefore print warning:
-        if atom.id != numall - 1:
-            warn("gxread: atom indicies are not sequential")
-
-        # append new atom to the list:
-        atoms.append(atom)
-    #end for
+    # convert to 2D array:
+    xyz = nx3(xyz)
+    # print "xyz=", xyz
 
     #
     # Second, read in energy:
@@ -266,7 +96,6 @@ def gxread(file):
             energy = float( fields[0] )
         except StopIteration: # is raised by .next() on EOF
             warn("gxread: gxfile does not contain energy")
-            energy = None
             raise EOF, "gxread: no energies"
             # or maybe "return atoms, loop, None" right away?
         #end try
@@ -274,25 +103,23 @@ def gxread(file):
         #
         # Third, read in forces:
         #
-        for atom in atoms:
-            if is_dummy(atom):
-                # fake entry with zero gradients:
-                atom._parse_grad("0 0.0 0.0 0.0")
-            else:
-                atom._parse_grad(lines.next())
-            #end if
-        #end for
+
+ 	# parse the rest of the file:
+	grads = [ _parse_grad(line) for line in lines ]
+
+	# convert to 2D array:
+	grads = nx3(grads)
+	# print "grads=", grads
     except EOF:
-        warn("gxread: gxfile incomplete")
+	energy = None
+	grads = None
     #end try
 
-# # debug print:
-# for atom in atoms:
-#   print(atom)
-    return atoms, loop, energy
+    # return all columns and scalar energy:
+    return atnums, xyz, isyms, inums, iconns, ivars, grads, energy 
 #end def
 
-def gxwrite(file,atoms,loop=1,energy=None):
+def gxwrite(atnums, positions, isyms, inums, iconns, ivars, grads=None, energy=None, loop=1, file='-'):
     """Write gxfile in following format,
     (first and last lines are not parts of the file, rather for field width extimation):
     #12345 1234567890123456789012 1234567890123456789012 1234567890123456789012 123 123   123 123 123   123 123 123
@@ -326,8 +153,11 @@ def gxwrite(file,atoms,loop=1,energy=None):
     #
     # First, the geometry section ...
     #
-    for a in atoms:
-        write( a._tostr_geom() )
+    cols = (atnums, positions, isyms, inums, iconns, ivars)
+    rows = zip( *cols )
+
+    for row in rows: # row = (atnum, pos, isym, inum, iconn, ivar)
+        write( _tostr_geom( *row ) )
     #end for
 
     #
@@ -340,15 +170,104 @@ def gxwrite(file,atoms,loop=1,energy=None):
     #
     # Second, the energy and gradients (not needed if this is a new geometry):
     #
+
+    # energy:
     write( " %23.12f %23.12f\n"  % (energy,energy) )
 
-    for a in atoms:
-        if is_dummy(a): continue
-        if not hasattr(a,"grad"): break
-        # FIXME: the case when only some of atoms dont have forces
-        write( a._tostr_grad() )
-    #end for
+    # gradients:
+    for inum, grad in enumerate(grads):
+	# enumerate starts from zero:
+	write( _tostr_grad(inum+1, grad) )
 #end def
+
+def _tostr_geom(atnum, pos, isym, inum, iconn, ivar):
+    """Return a line of gxfile in following format:
+    ^ 1.00        -5.184294677816         2.993153927796         0.427911418660   5  18     6   1   3     4   7   0
+    """
+
+    # extract the fields from records:
+
+    if is_dummy(atnum):
+	# gxfile convention for dummy atoms:
+	atnum = 99.0
+    else:
+	atnum = float(atnum)
+
+    # concatenate tuples, the first one is a singleton:
+    fields = (atnum,) + tuple(pos) + (isym, inum) + tuple(iconn) + tuple(ivar)
+
+    return ( "%5.2f %22.12f %22.12f %22.12f %3i %3i   %3i %3i %3i   %3i %3i %3i\n" \
+		 % fields )
+#end def
+
+def _tostr_grad(inum, grad):
+    """Return a line of gxfile containg cartesian gradients:
+    ^   20       -0.000000596917  -0.000000344630   0.000002806462
+    """
+    #
+    # WARNING: all the numerical ids are rebased to 1,
+    #          so that those (invalid) -1s become 0.
+    #
+
+    # conversion to floats is for the case when vector components are DerivVars:
+    grad = tuple( map(float, grad) )
+
+    # concatenate tuples, the first one is a singleton:
+    fields = (inum,) + grad
+    return ( "%5i      %16.12f %16.12f %16.12f\n" % fields )
+#end def
+
+def _parse_geom(line):
+    """Parse a line of gxfile in following format:
+    ^ 1.00        -5.184294677816         2.993153927796         0.427911418660   5  18     6   1   3     4   7   0
+    """
+    fields = line.split()
+    # print(fields)
+
+    # nuclear charge, may even be fractional, for dummy atoms Z=99:
+    atnum = int( float( fields[0] ) )
+
+    # gxfile convention for dummy atoms:
+    if atnum == 99.0: atnum = DUMMY
+
+    # exit on negative charge:
+    if atnum < 0:
+	# this is the only usefull field in this line,
+	# but there is no use for it here ...
+	return ( atnum, None, None, None, None, None )
+
+    # 3D vector of atomic position:
+    pos    = [ float(i) for i in fields[1:4] ] # yes, three fields 1 <= xyz < 4
+
+    # Vrap 3D vectors in vector type, see "import ... as V" at the top:
+    pos = V(pos)
+
+    # uniqie atom id, indexing groups of symmetry equivalent atoms:
+    isym = int( fields[4] )
+
+    # numeric ID used to define connectiviites:
+    inum   = int( fields[5] )
+
+    # connectivities:
+    iconn  = [ int(i) for i in fields[6:9] ] # three fields
+
+    # integer lables for "variable" internal coordinates,
+    # zero for "constrained" internal coordinates:
+    ivar  = [ int(i) for i in fields[9:12] ] # three fields
+
+    return ( atnum, pos, isym, inum, iconn, ivar )
+#enddef
+
+def _parse_grad(line):
+    """Parse a line of gxfile containg cartesian gradients:
+    ^   20       -0.000000596917  -0.000000344630   0.000002806462
+    """
+    fields = line.split()
+    grad = map( float, fields[1:4] )
+
+    # Vrap 3D vectors in vector type, see "import ... as V" at the top:
+    return V(grad)
+#enddef
 
 # You need to add "set modeline" and eventually "set modelines=5"
 # to your ~/.vimrc for this to take effect.
